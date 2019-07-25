b5566e7aa1196f6ef824444a007b543f85a89f93
Anthony Diaz <anthony.diaz2@unmsm.edu.pe>
2019-07-25 15:08:09 -0500

    Seccion noticias y menu-responsive v2.0

--- docs/js/menuResponsive.js
+++ docs/js/menuResponsive.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
     $('#check').on('change', function(e){
         e.preventDefault();
         if (this.checked) {
-            $(".menu-responsive").css({"z-index": "2"});
+            $(".menu-responsive nav").css({"z-index": "2"});
         } else {
             setTimeout(function(){
-                $(".menu-responsive").css({"z-index": "0"});
+                $(".menu-responsive nav").css({"z-index": "0"});
               }, 2000);
             
         }
